fe89d883f443ff1c736a72356511cc51ee3e8c7b
zzlgithub <947502908@qq.com>
2023-10-11 18:07:14 +0800

    测试提交

--- app/src/androidTest/java/com/nan/shopping/ApplicationTest.java
+++ app/src/androidTest/java/com/nan/shopping/ApplicationTest.java
@@ -8,6 +8,7 @@ import android.test.ApplicationTestCase;
  */
 public class ApplicationTest extends ApplicationTestCase<Application> {
     public ApplicationTest() {
+        //测试提交
         super(Application.class);
     }
 }
